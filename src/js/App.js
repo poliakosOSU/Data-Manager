902fc0d48ec7095e7a990dc6ccfc0899b9236030
poliakosOSU <poliakos@oregonstate.edu>
2021-03-31 12:16:20 -0700

    Added new material UI component  Please enter the commit message for your changes. Lines starting

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MenuBar from './components/menuBar';
 import DenseAppBar from './components/denseAppBar';
+import MediaCard from './components/mediaCard';
 // import { electron } from 'webpack';
 
 function App() {
     return (
+        <div>
         <DenseAppBar />
+        <MediaCard />
+        </div>
         // <div>
         // <h1>React Component</h1>
         // <button onClick={() => {
